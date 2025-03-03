be1b7cb7ed56578bbe91199b42ae8f114632a666
ro-jo-mo <robert.moran147@gmail.com>
2025-03-03 12:46:29 +0000

    upd

---
+++ alpha-beta-CROWN/complete_verifier/custom/my_models.py
@@ -0,0 +1,33 @@
+import torch
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        x = torch.zeros((64,3,_N,_N))
+        self.activation = torch.nn.functional.relu
+        self.pool = torch.nn.MaxPool2d(2,2)
+
+        self.conv1 = torch.nn.Conv2d(3,6,5)
+        x = self.pool(self.activation(self.conv1(x)))
+        
+        self.conv2 = torch.nn.Conv2d(x.shape[1],16,5)
+        x = self.pool(self.activation(self.conv2(x)))
+        x = torch.flatten(x, start_dim=1)
+        self.dense1 = torch.nn.Linear(x.shape[1],128)
+        x = self.activation(self.dense1(x))
+        self.dense2 = torch.nn.Linear(x.shape[1],64)
+        x = self.activation(self.dense2(x))
+        self.final = torch.nn.Linear(x.shape[1],n_classes)
+
+
+    def forward(self,x):
+        x = self.pool(self.activation(self.conv1(x)))
+        x = self.pool(self.activation(self.conv2(x)))
+        
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.activation(self.dense1(x))
+        x = self.activation(self.dense2(x))
+        x = self.final(x)
+
+        return x
