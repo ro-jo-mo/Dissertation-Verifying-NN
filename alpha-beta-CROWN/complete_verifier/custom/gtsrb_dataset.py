c8382b0eb75defe7217f9fefe4dd6222da05d466
ro-jo-mo <robert.moran147@gmail.com>
2025-03-06 14:13:44 +0000

    updates

--- alpha-beta-CROWN/complete_verifier/custom/gtsrb_dataset.py
+++ alpha-beta-CROWN/complete_verifier/custom/gtsrb_dataset.py
@@ -16,17 +16,46 @@ def gtsrb_32_colour(spec):
         transforms.ToTensor(),
         normalise
     ])
-    if spec["epsilon"] is None:
+    epsilon = spec["epsilon"]
+    if epsilon is None:
         raise ValueError("You must specify an epsilon")
     # Load full dataset 
     dataset = torchvision.datasets.ImageFolder(root=PATH, transform=transform)
     loader = torch.utils.data.DataLoader(dataset, 
-                                           batch_size=10, 
+                                           batch_size=99999, 
                                            num_workers=6,
                                            shuffle=False
                                            )
     images,labels = next(iter(loader))
     minimum_values = normalise(torch.zeros((1,3,1,1)))
     maximum_values = normalise(torch.ones((1,3,1,1)))
+    epsilon
     epsilon = (epsilon / std).reshape(1,3,1,1)
+    return images, labels, maximum_values, minimum_values, epsilon
+
+def gtsrb_32_grey(spec):
+    mean = torch.tensor(arguments.Config["data"]["mean"])
+    std = torch.tensor(arguments.Config["data"]["std"])
+    normalise = transforms.Normalize(mean, std)
+    transform = transforms.Compose([
+        transforms.Resize((32,32)),
+        transforms.ToTensor(),
+        transforms.Grayscale(),
+        normalise
+    ])
+    epsilon = spec["epsilon"]
+    if epsilon is None:
+        raise ValueError("You must specify an epsilon")
+    # Load full dataset 
+    dataset = torchvision.datasets.ImageFolder(root=PATH, transform=transform)
+    loader = torch.utils.data.DataLoader(dataset, 
+                                           batch_size=99999, 
+                                           num_workers=6,
+                                           shuffle=False
+                                           )
+    images,labels = next(iter(loader))
+    minimum_values = normalise(torch.zeros((1,1,1,1)))
+    maximum_values = normalise(torch.ones((1,1,1,1)))
+    epsilon
+    epsilon = (epsilon / std).reshape(1,1,1,1)
     return images, labels, maximum_values, minimum_values, epsilon
--- alpha-beta-CROWN/complete_verifier/custom/my_models.py
+++ alpha-beta-CROWN/complete_verifier/custom/my_models.py
@@ -1,13 +1,14 @@
 import torch
 
-class Model(torch.nn.Module):
+class Model_Colour(torch.nn.Module):
     def __init__(self):
-        super().__init__()
         n_classes = 43
         _N = 32
+        super().__init__()
+
         x = torch.zeros((64,3,_N,_N))
         self.activation = torch.nn.functional.relu
-        self.pool = torch.nn.MaxPool2d(2,2)
+        self.pool = torch.nn.AvgPool2d(2,2)
 
         self.conv1 = torch.nn.Conv2d(3,6,5)
         x = self.pool(self.activation(self.conv1(x)))
@@ -33,5 +34,43 @@ class Model(torch.nn.Module):
         x = self.final(x)
 
         return x
-def my_model():
-    return torch.compile(Model())
+def model_colour():
+    return Model_Colour()
+
+class Model_Grey(torch.nn.Module):
+    def __init__(self):
+        n_classes = 43
+        _N = 32
+        super().__init__()
+        x = torch.zeros((64,1,_N,_N))
+
+        self.activation = torch.nn.functional.relu
+
+        self.pool = torch.nn.AvgPool2d(2,2)
+
+        self.conv1 = torch.nn.Conv2d(1,6,5)
+        x = self.pool(self.activation(self.conv1(x)))
+        
+        self.conv2 = torch.nn.Conv2d(x.shape[1],16,5)
+        x = self.pool(self.activation(self.conv2(x)))
+        x = torch.flatten(x, start_dim=1)
+        self.dense1 = torch.nn.Linear(x.shape[1],128)
+        x = self.activation(self.dense1(x))
+        self.dense2 = torch.nn.Linear(x.shape[1],64)
+        x = self.activation(self.dense2(x))
+        self.final = torch.nn.Linear(x.shape[1],n_classes)
+
+
+    def forward(self,x):
+        x = self.pool(self.activation(self.conv1(x)))
+        x = self.pool(self.activation(self.conv2(x)))
+        
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.activation(self.dense1(x))
+        x = self.activation(self.dense2(x))
+        x = self.final(x)
+
+        return x
+def model_grey():
+    return Model_Grey()
