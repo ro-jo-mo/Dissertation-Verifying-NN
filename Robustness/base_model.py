c0406fc7ad40c46cad94f51a992770921cee808c
ro-jo-mo <robert.moran147@gmail.com>
2025-03-17 12:35:52 +0000

    update

---
+++ Robustness/base_model.py
@@ -0,0 +1,246 @@
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_curve, roc_auc_score
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+from torchvision import transforms
+import numpy as np
+from pathlib import Path
+from torchsummary import summary
+torch.set_printoptions(sci_mode=False)
+
+PATH = Path("/home/rob/code/Project/dataset/GTSRB/Training")
+torch.set_float32_matmul_precision('high')
+LABELS = ["20 km/h"
+,"30 km/h"
+,"50 km/h"
+,"60 km/h"
+,"70 km/h"
+,"80 km/h"
+,"End of speed limit"
+,"100 km/h"
+,"120 km/h"
+,"No passing"
+,"No passing for large vehicles"
+,"Right of way"
+,"Priority road"
+,"Yield"
+,"Stop"
+,"No vehicles"
+,"Large vehicles prohibited"
+,"No entry"
+,"General caution"
+,"Dangerous curve to the left"
+,"Dangerous curve to the right"
+,"Double curve"
+,"Bumpy road"
+,"Slippery road"
+,"Road narrows"
+,"Road work"
+,"Traffic signals"
+,"Pedestrians"
+,"Children crossing"
+,"Bicycle crossing"
+,"Ice warning"
+,"Wild animals crossing"
+,"End of all speed and passing limits"
+,"Turn right ahead"
+,"Turn left ahead"
+,"Ahead only"
+,"Go straight or right"
+,"Go straight or left"
+,"Keep right"
+,"Keep left"
+,"Roundabout mandatory"
+,"End of no passing"
+,"End of no passing by large vehicles"]
+
+_N = 32
+batch_size = 128
+n_classes = 43
+epochs = 30
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+mean = 0.3211
+std = 0.2230
+
+def to_image(img): # convert to unormalized form for viewing
+
+    return (img * std + mean).permute(1,2,0).numpy()
+
+normalise = transforms.Normalize(mean, std)
+transform = transforms.Compose([
+    transforms.Resize((_N,_N)),
+    transforms.ToTensor(),
+    transforms.Grayscale(),
+    normalise
+])
+
+dataset = torchvision.datasets.ImageFolder(root=PATH, transform=transform)
+train_dataset, test_dataset = torch.utils.data.random_split(dataset, [0.8,0.2])
+train_loader = torch.utils.data.DataLoader(train_dataset, 
+                                           batch_size=batch_size, 
+                                           num_workers=6,
+                                           shuffle=True
+                                           )
+test_loader = torch.utils.data.DataLoader(test_dataset, 
+                                          batch_size=batch_size, 
+                                          num_workers=6,
+                                          shuffle=False
+                                          )
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        x = torch.zeros((64,1,_N,_N))
+
+        self.activation = torch.nn.functional.relu
+
+        self.pool = torch.nn.AvgPool2d(2,2)
+
+        self.conv1 = torch.nn.Conv2d(1,6,5)
+        x = self.pool(self.activation(self.conv1(x)))
+        
+        self.conv2 = torch.nn.Conv2d(x.shape[1],16,5)
+        x = self.pool(self.activation(self.conv2(x)))
+        x = torch.flatten(x, start_dim=1)
+        self.dense1 = torch.nn.Linear(x.shape[1],128)
+        x = self.activation(self.dense1(x))
+        self.dense2 = torch.nn.Linear(x.shape[1],64)
+        x = self.activation(self.dense2(x))
+        self.final = torch.nn.Linear(x.shape[1],n_classes)
+
+
+    def forward(self,x):
+        x = self.pool(self.activation(self.conv1(x)))
+        x = self.pool(self.activation(self.conv2(x)))
+        
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.activation(self.dense1(x))
+        x = self.activation(self.dense2(x))
+        x = self.final(x)
+
+        return x
+
+
+def train(model,loss_func,optimiser):
+    for epoch in range(epochs):
+        total_loss = 0
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            optimiser.zero_grad()
+            preds = model(images)
+            loss = loss_func(preds, labels)
+            loss.backward()
+            optimiser.step()
+            total_loss += loss.item()
+        print(f"Epoch {epoch+1} --- Training Loss {total_loss / len(train_loader):.3f} --- Validation Loss {test_performance(model,loss_func):.3f}")
+
+def test_performance(model,loss_func):
+    loss = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+            preds = model(images)
+            loss += loss_func(preds,labels).item()
+    return loss / len(test_loader)
+
+def evaluate(model):
+    predictions = []
+    labels = []
+    with torch.no_grad():
+        for images,lbls in test_loader:
+            labels.extend(lbls.numpy())
+            
+            images = images.to(device)
+            preds = model(images)
+            predictions.extend(preds.cpu().numpy())
+            
+    labels = np.array(labels)
+    predictions = np.array(predictions)
+    pred_class = np.argmax(predictions,axis=1)
+    precision = precision_score(labels, pred_class, average="weighted")
+    recall = recall_score(labels, pred_class, average="weighted")
+    f1 = f1_score(labels, pred_class, average="weighted")
+    print(f'Precision (macro): {precision:.4f}')
+    print(f'Recall (macro): {recall:.4f}')
+    print(f'F1-score (macro): {f1:.4f}')
+
+@torch.compile
+def pgd_attack(model,images,labels,loss_func,iterations,epsilon,alpha):
+    
+    adversarial = denormalise(images)
+    lower_bound = adversarial - epsilon
+    upper_bound = adversarial + epsilon
+    
+    decay = torch.logspace(alpha,1,iterations,2)
+    decay = decay / decay[0]
+
+    for alpha in decay:
+        normalised = normalise(adversarial)
+        normalised.requires_grad = True
+        model.zero_grad()
+        pred = model(normalised)
+        loss = loss_func(pred,labels)
+        #print(loss.item())
+        loss.backward()
+        # TODO - Edit Learning Rate / Apply Adam Optimiser
+        perturbations = torch.sign(normalised.grad.data) * epsilon * alpha
+        adversarial += perturbations
+        adversarial = torch.clip(adversarial,lower_bound, upper_bound)
+        adversarial = torch.clip(adversarial,0,1)
+
+    return normalise(adversarial)
+
+
+normalise.to(device)
+
+@torch.compile
+def denormalise(images):
+    return images * std + mean
+
+def adversarial_training(model,loss_func,optimiser,epochs,iterations,epsilon,alpha):
+    for epoch in range(epochs):
+        total_loss = 0
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            images = torch.cat((images,
+                pgd_attack(model,images,labels,loss_func,iterations,epsilon,alpha)
+                ))
+            labels = torch.cat((labels,labels))
+
+            optimiser.zero_grad()
+            preds = model(images)
+            loss = loss_func(preds, labels)
+            loss.backward()
+            optimiser.step()
+            total_loss += loss.item()
+        print(f"Epoch {epoch+1} --- Training Loss {total_loss / len(train_loader):.3f} --- Validation Loss {test_performance(model,loss_func):.3f}")
+
+
+if __name__ == "main":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epsilon",default=0)
+    parser.add_argument("--epochs",default=40)
+    args = parser.parse_args()
+    epsilon = args.epsilon
+    epochs = args.epochs
+    alpha = 2
+    iterations = 20
+    model = Model()
+    loss_func = torch.nn.CrossEntropyLoss().to(device)
+    optimiser = torch.optim.Adam(model.parameters())   
+    if epsilon == 0:
+        train(model,loss_func,optimiser)
+    else:
+        adversarial_training()
+        # Idea -> Train 
+
+    
