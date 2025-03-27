ececd871ad1b19e936183eadd074ec3f8f306c3c
ro-jo-mo <robert.moran147@gmail.com>
2025-03-27 13:58:53 +0000

    updates

--- alpha-beta-CROWN/complete_verifier/custom/my_models.py
+++ alpha-beta-CROWN/complete_verifier/custom/my_models.py
@@ -73,3 +73,41 @@ class Model_Grey(torch.nn.Module):
         return x
 def model_grey():
     return Model_Grey()
+
+
+
+class Superclass(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        _N = 32
+        x = torch.zeros((64,1,_N,_N))
+        n_classes = 12
+        self.activation = torch.nn.functional.relu
+
+        self.pool = torch.nn.MaxPool2d(2,2)
+
+        self.conv1 = torch.nn.Conv2d(1,6,5)
+        x = self.pool(self.activation(self.conv1(x)))
+        
+        self.conv2 = torch.nn.Conv2d(x.shape[1],16,5)
+        x = self.pool(self.activation(self.conv2(x)))
+        x = torch.flatten(x, start_dim=1)
+        self.dense1 = torch.nn.Linear(x.shape[1],128)
+        x = self.activation(self.dense1(x))
+        self.dense2 = torch.nn.Linear(x.shape[1],64)
+        x = self.activation(self.dense2(x))
+        self.final = torch.nn.Linear(x.shape[1],n_classes)
+
+    def forward(self,x):
+        x = self.pool(self.activation(self.conv1(x)))
+        x = self.pool(self.activation(self.conv2(x)))
+        
+        x = torch.flatten(x, start_dim=1)
+
+        x = self.activation(self.dense1(x))
+        x = self.activation(self.dense2(x))
+        x = self.final(x)
+
+        return x
+def superclass():
+    return Superclass()
