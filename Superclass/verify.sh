62ae9d7a71e912ffb6347310c5eefb5af3cf6bf5
ro-jo-mo <robert.moran147@gmail.com>
2025-04-28 23:23:30 +0100

    updates

--- Superclass/verify.sh
+++ Superclass/verify.sh
@@ -2,13 +2,7 @@ cd ..
 cd alpha-beta-CROWN
 cd complete_verifier
 
-models="../../Superclass/models/"
-for model in "$models"*.pth; do
-    file=$(basename "$model")
-    echo "File: $file"
-    echo "Path: $model"
-    results=$file"_superclass.pkl"
-    echo "Results: $results"
-    python abcrown.py --config exp_configs/my_configs/superclass.yaml --results_file "$results" --csv_name "instances_eps_5.csv" --load_model "$model"
-done
-# python abcrown.py --config exp_configs/my_configs/superclass.yaml --results_file "constraint_only_10_eps" --csv_name "instances_eps_5.csv" --load_model "../../Superclass/models/constraint_only_10_eps.pth"
+
+python abcrown.py --config exp_configs/my_configs/superclass.yaml --results_file "5_eps_constraint_only.pkl" --csv_name "instances_eps_5.csv" --load_model "../../Superclass/models/constraint_only_10_eps.pth"
+python abcrown.py --config exp_configs/my_configs/superclass.yaml --results_file "5_eps_hybrid.pkl" --csv_name "instances_eps_5.csv" --load_model "../../Superclass/models/hybrid_10_eps.pth"
+python abcrown.py --config exp_configs/my_configs/superclass.yaml --results_file "5_eps_base.pkl" --csv_name "instances_eps_5.csv" --load_model "../../Superclass/models/superclass_base.pth"
